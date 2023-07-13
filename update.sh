d8dc7449c167154a1398064edc617bd2b2cf35ac
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-07-13 16:35:21 -0400

    test prep

--- update.sh
+++ update.sh
@@ -18,6 +18,20 @@ fi
 echo "Local IP: $IP"
 echo "Local PORT: $PORT"
 
+# Define the port numbers to free up
+PORT_NUMBERS=(7070 7071 7072 7078 7087)
+
+# Loop through the port numbers and kill the processes using each port
+for PORT_NUMBER in "${PORT_NUMBERS[@]}"; do
+  PIDS=$(lsof -t -i:"$PORT_NUMBER")
+  if [ -z "$PIDS" ]; then
+    echo "No processes found using port $PORT_NUMBER."
+  else
+    echo "Killing processes using port $PORT_NUMBER..."
+    kill $PIDS
+  fi
+done
+
 go build
 
 if [ "$1" = "master" ]; then
