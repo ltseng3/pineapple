15c4e6633dbcde4094ad025f4d45e52a0ba075e2
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-08-15 10:59:22 -0400

    updated test

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-timeout 180s go run client/client.go -maddr="10.10.1.1" -writes=0.475 -rmws=0.05 -c=0 -T=300
+timeout 180s go run client/client.go -maddr="10.10.1.1" -writes=0.495 -rmws=0.01 -c=25 -T=80
 python3 client_metrics.py
