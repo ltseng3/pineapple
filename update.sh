806f0ec33da7e987f4a2d71d5bb815c7fa179e6e
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-08-15 10:57:07 -0400

    5 node support

--- update.sh
+++ update.sh
@@ -19,12 +19,18 @@ else
   elif [ $HOST = "node-2" ]; then
       IP="10.10.1.3"
       PORT=7072
+  elif [ $HOST = "node-3" ]; then
+        IP="10.10.1.4"
+        PORT=7073
+  elif [ $HOST = "node-4" ]; then
+        IP="10.10.1.5"
+        PORT=7074
   fi
   echo "Local IP: $IP"
   echo "Local PORT: $PORT"
 
   # Define the port numbers to free up
-  PORT_NUMBERS=(7070 7071 7072 7078 7087)
+  PORT_NUMBERS=(7070 7071 7072 7073 7074 7078 7087)
 
   # Loop through the port numbers and kill the processes using each port
   for PORT_NUMBER in "${PORT_NUMBERS[@]}"; do
@@ -38,7 +44,7 @@ else
   done
 
   if [ "$1" = "master" ]; then
-    ./program -maddr "10.10.1.1" -N 3 &
+    ./program -maddr "10.10.1.1" -N 5 &
     sleep 0.5
   fi
 
