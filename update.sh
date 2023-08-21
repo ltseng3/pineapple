7869d184c8b1ff383451ede1f6b1c7da218d6f22
tigranb2 <tigran@Tigrans-Laptop.local>
2023-08-21 14:39:05 -0400

    updated GOBIN

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ cd ../pineapple || exit
 git stash && git stash clear && git pull
 
 export GOPATH=~/go/src/
-export GOBIN=~/go/src/pineapple
+export GOBIN=~/go/src/pineapple/bin
 go install pineapple/src/master
 go install pineapple/src/server
 go install pineapple/src/client
