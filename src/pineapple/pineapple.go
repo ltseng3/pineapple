e4a0c70eae1c39dd5de3fc7ed0437e2fd4480698
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-15 11:36:29 -0500

    send reads/writes to all servers

--- src/pineapple/pineapple.go
+++ src/pineapple/pineapple.go
@@ -223,9 +223,6 @@ func (r *Replica) bcastGet(instance int32, write bool, key int) {
 		if !r.Alive[q] {
 			continue
 		}
-		if q == 0 { // don't send read/write messages to leader
-			continue
-		}
 
 		r.SendMsg(q, r.getRPC, args)
 	}
@@ -352,9 +349,6 @@ func (r *Replica) bcastSet(instance int32, write bool, key int, payload pineappl
 			break
 		}
 		if !r.Alive[q] {
-			continue
-		}
-		if q == 0 { // don't send read/write messages to leader
 			continue
 		}
 
@@ -726,6 +720,7 @@ func (r *Replica) Run() {
 	go r.WaitForClientConnections()
 
 	if r.Id == 0 {
+		r.IsLeader = true
 		go r.executeRMWs()
 	}
 
