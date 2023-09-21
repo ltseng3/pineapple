9a7489391c012aea55db862e2a763a0d2830b7fd
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-20 22:58:17 -0500

    added client new install

--- compile.sh
+++ compile.sh
@@ -3,4 +3,5 @@ export GOBIN=/root/go/src/pineapple/bin
 go install pineapple/src/master
 go install pineapple/src/server
 go install pineapple/src/client
+go install pineapple/src/clientnew
 export GOBIN=
