429e645ac63d9e040284aacbc81a92dc33810a42
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-20 23:17:54 -0500

    removed unused files

--- src/clientnew/clientnew.go
+++ src/clientnew/clientnew.go
@@ -396,33 +396,33 @@ func printer(readings chan *response) {
 
 func printerMultipleFile(readings chan *response, replicaID int, experimentStart time.Time, rampDown, rampUp, timeout *int) {
 	fileName := fmt.Sprintf("lattput-%d.txt", replicaID)
-	lattputFile, err := os.Create(fileName)
-	if err != nil {
-		log.Println("Error creating lattput file", err)
-		return
-	}
-
-	fileName = fmt.Sprintf("latFileRead-%d.txt", replicaID)
-	latFileRead, err := os.Create(fileName)
-	if err != nil {
-		log.Println("Error creating latency file", err)
-		return
-	}
-	//latFile.WriteString("# time (ns), latency, commit latency\n")
-
-	fileName = fmt.Sprintf("latFileWrite-%d.txt", replicaID)
-	latFileWrite, err := os.Create(fileName)
-	if err != nil {
-		log.Println("Error creating latency file", err)
-		return
-	}
-
-	fileName = fmt.Sprintf("latFileRMW-%d.txt", replicaID)
-	latFileRMW, err := os.Create(fileName)
-	if err != nil {
-		log.Println("Error creating latency file", err)
-		return
-	}
+	//lattputFile, err := os.Create(fileName)
+	//if err != nil {
+	//	log.Println("Error creating lattput file", err)
+	//	return
+	//}
+	//
+	//fileName = fmt.Sprintf("latFileRead-%d.txt", replicaID)
+	//latFileRead, err := os.Create(fileName)
+	//if err != nil {
+	//	log.Println("Error creating latency file", err)
+	//	return
+	//}
+	////latFile.WriteString("# time (ns), latency, commit latency\n")
+	//
+	//fileName = fmt.Sprintf("latFileWrite-%d.txt", replicaID)
+	//latFileWrite, err := os.Create(fileName)
+	//if err != nil {
+	//	log.Println("Error creating latency file", err)
+	//	return
+	//}
+	//
+	//fileName = fmt.Sprintf("latFileRMW-%d.txt", replicaID)
+	//latFileRMW, err := os.Create(fileName)
+	//if err != nil {
+	//	log.Println("Error creating latency file", err)
+	//	return
+	//}
 
 	fileName = fmt.Sprintf("latFileMAX-%d.txt", replicaID)
 	latFileMAX, err := os.Create(fileName)
@@ -446,11 +446,14 @@ func printerMultipleFile(readings chan *response, replicaID int, experimentStart
 			// Log all to latency file if they are not within the ramp up or ramp down period.
 			if *rampUp < int(currentRuntime.Seconds()) && int(currentRuntime.Seconds()) < *timeout-*rampDown {
 				if resp.operation == state.GET {
-					latFileRead.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
+					fmt.Println(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
+					//latFileRead.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
 				} else if resp.operation == state.PUT {
-					latFileWrite.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
+					fmt.Println(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
+					//latFileWrite.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
 				} else if resp.operation == state.RMW { // rmw
-					latFileRMW.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
+					fmt.Println(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
+					//latFileRMW.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.rtt, resp.commitLatency))
 				} else { // max
 					latFileMAX.WriteString(fmt.Sprintf("%d %f %f\n", resp.receivedAt.UnixNano(), resp.commitLatency, resp.rtt))
 				}
@@ -480,7 +483,8 @@ func printerMultipleFile(readings chan *response, replicaID int, experimentStart
 		//lattputFile.WriteString(fmt.Sprintf("%d %f %f %d %d %f\n", endTime.UnixNano(), avg, tput, count, totalOrs, avgCommit))
 		// Log all to latency file if they are not within the ramp up or ramp down period.
 		if *rampUp < int(currentRuntime.Seconds()) && int(currentRuntime.Seconds()) < *timeout-*rampDown {
-			lattputFile.WriteString(fmt.Sprintf("%d %f %f %d %d %f\n", endTime.UnixNano(), avg, tput, count, totalOrs, avgCommit))
+			fmt.Println(fmt.Sprintf("%d %f %f %d %d %f\n", endTime.UnixNano(), avg, tput, count, totalOrs, avgCommit))
+			//lattputFile.WriteString(fmt.Sprintf("%d %f %f %d %d %f\n", endTime.UnixNano(), avg, tput, count, totalOrs, avgCommit))
 		}
 		startTime = endTime
 	}
